1e6c0d92da45a469d353530848bbad53cab5c7e6
Bhupender Singh <38017615+bhupendersinghh@users.noreply.github.com>
2020-01-04 12:50:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,17 @@ app.get("/",  (req, res) => {
 
 server = app.listen(port, process.env.IP);
 
-const io = require("socket.io")(server, { origins: '*:*'});
+const io = require("socket.io")(server, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+});
 
 io.on("connection", (socket) => {
 
