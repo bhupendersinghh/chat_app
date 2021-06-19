aa745d791504faa6a3f144392dac5445432bbd0e
Bhupender Singh <38017615+bhupendersinghh@users.noreply.github.com>
2021-06-20 00:14:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.set("view engine", "ejs");
 
 //Directory for middlewares
 app.use(express.static("public"));
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+});
 
 //Routes
 app.get("/",  (req, res) => {
